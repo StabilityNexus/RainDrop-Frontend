99d8f61cec225544c5594c3adc5600321f6124cf
DengreSarthak <dengresarthak2002@gmail.com>
2025-08-11 13:40:25 +0530

    Abi changes

--- src/utils/contractAddress.ts
+++ src/utils/contractAddress.ts
@@ -1,5 +1,5 @@
 export const RaindropFractoryAddress = {
-534351: '0x4ae8eb66124e7fd311edd5e818b33103bc5aeea7',
+534351: '0xb7564ba25889043bdd0ee8a1a3adcaf2e23e3e09',
 } as {
   [key: number]: `0x${string}`
 }
